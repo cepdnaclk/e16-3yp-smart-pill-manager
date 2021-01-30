12e1735eb44855457a1f80048ab6e357ee96eef6
arunanuwantha97 <arunanuwanthablan97@gmail.com>
2021-01-30 16:29:08 +0530

    protecting routes

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cb68b0167598f8b1f9cb01829ff4feb.js"
+  "/precache-manifest.98c04cbc2eedf2f6b4275eb5121a041d.js"
 );
 
 self.addEventListener('message', (event) => {
