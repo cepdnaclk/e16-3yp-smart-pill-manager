6c3bbff8a761e20737f1d83b943523870fdbfedf
Aruna Nuwantha <e16261@eng.pdn.ac.lk>
2021-05-25 10:35:34 +0530

    update the web page

--- docs/assets/js/bs-init.js
+++ docs/assets/js/bs-init.js
@@ -1,3 +1,8 @@
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate');
+}
+
 $(document).ready(function(){
+	AOS.init();
 	$('[data-bss-tooltip]').tooltip();
 });
